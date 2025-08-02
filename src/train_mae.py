cde34b8d506acd846178347cd11af7f1f475d134
Akarsh Kumar <akarshkumar0101@gmail.com>
2025-08-02 04:18:34 +0000

    sync

--- src/train_mae.py
+++ src/train_mae.py
@@ -3,7 +3,6 @@ import os
 import copy
 from functools import partial
 from dataclasses import dataclass
-from typing import List
 from tqdm import tqdm
 import tyro
 
@@ -14,21 +13,17 @@ from jax.random import split
 import optax
 from flax.training.train_state import TrainState
 from flax import linen as nn
+from einops import rearrange
 
 from substrates.gol import GameOfLife
 from rollout import rollout_simulation
 
-from models.mae import MAE, MAEConfig
-
 import util
 
-
 @dataclass
-class OptimizerArgs:
-    batch_size: int = 32
-    learning_rate: float = 3e-4
-    weight_decay: float = 0.
-    clip_grad_norm: float = 1.0
+class ModelArgs:
+    d_hidden: int = 1024
+    layers: int = 8
 
 @dataclass
 class DataArgs:
@@ -39,29 +34,44 @@ class DataArgs:
     t_end: int = 64
 
 @dataclass
+class OptimizerArgs:
+    batch_size: int = 32
+    grad_accum_steps: int = 1
+    clip_grad_norm: float = 1.0
+    learning_rate: float = 3e-4
+    weight_decay: float = 1e-4
+    beta1: float = 0.9
+    beta2: float = 0.999
+    eps: float = 1e-8
+
+    decay_lr: bool | None = True
+
+@dataclass
 class Args:
     seed: int = 0
     save_dir: str | None = None
     n_iters: int = 10000
     log_every: int = 1000
 
-    model: MAEConfig = MAEConfig(nt=4, nh=8, nw=8, pt=8, ph=8, pw=8)
-    opt: OptimizerArgs = OptimizerArgs()
+    model: ModelArgs = ModelArgs()
     data: DataArgs = DataArgs()
+    opt: OptimizerArgs = OptimizerArgs()
 
-class Main:
-    def __init__(self, args: Args):
-        args = copy.deepcopy(args)
+class Model(nn.Module):
+    @nn.compact
+    def __call__(self, inputs):
+        x, y = inputs['x'], inputs['y']
+        y_pred = nn.Dense(features=len(y))(x)
+        loss = ((y_pred - y)**2).mean()
+        outputs = dict(loss=loss, y_pred=y_pred, x=x, y=y)
+        return outputs
 
-        self.net = MAE(args.model)
-        self.substrate = GameOfLife(grid_size=args.data.grid_size)
-        self.rollout_fn = partial(rollout_simulation, s0=None, substrate=self.substrate, fm=None, rollout_steps=args.data.t_end+args.data.dt,
+class DataGenerator:
+    def __init__(self, args: DataArgs):
+        self.args = copy.deepcopy(args)
+        self.substrate = GameOfLife(grid_size=self.args.grid_size)
+        self.rollout_fn = partial(rollout_simulation, s0=None, substrate=self.substrate, fm=None, rollout_steps=self.args.t_end+self.args.dt,
                                   time_sampling='video', img_size=None, return_state=True)
-        self.args = args
-
-        self.generate_batch = jax.jit(self.generate_batch)
-        self.do_iter_train = jax.jit(partial(self.do_iter, train=True))
-        self.do_iter_eval = jax.jit(partial(self.do_iter, train=False))
     
     def generate_instance(self, rng):
         rng, _rng = split(rng)
@@ -75,31 +85,64 @@ class Main:
     def generate_batch(self, rng):
         return jax.vmap(self.generate_instance)(split(rng, self.args.opt.batch_size))
 
-    def loss_fn(self, params, batch):
-        outputs = jax.vmap(self.net.apply, in_axes=(None, 0, 0))(params, batch['rng'], batch['state'])
-        return outputs['loss_masked'].mean(), outputs
+class Main:
+    def __init__(self, args: Args):
+        self.args = copy.deepcopy(args)
 
+        self.model = Model(self.args.model)
+        self.data_gen = DataGenerator(self.args.data)
+        self.do_iter_train = jax.jit(self.do_iter_train)
+        self.do_iter_eval = jax.jit(self.do_iter_eval)
+
+    def loss_fn(self, params, batch):
+        inputs = dict(x=batch['state'], y=batch['state'])
+        outputs = jax.vmap(self.model.apply, in_axes=(None, 0, 0))(params, inputs)
+        return outputs['loss'].mean(), outputs
+    
     def do_iter(self, train_state, rng, train=True):
-        batch = self.generate_batch(rng)
-        (_, metrics), grads = jax.value_and_grad(self.loss_fn, has_aux=True)(train_state.params, batch)
+        batch = self.data_gen.generate_batch(rng)
+        batch = jax.tree.map(lambda x: rearrange(x, "(N B) ... -> N B ...", N=self.args.opt.grad_accum_steps), batch)
+        def micro_step(grads, batch):
+            (_, outputs), grads_new = jax.value_and_grad(self.loss_fn, has_aux=True)(train_state.params, batch)
+            grads = jax.tree.map(jnp.add, grads, grads_new)
+            return grads, outputs
+        grads = jax.tree.map(lambda x: jnp.zeros_like(x), train_state.params)
+        grads, outputs = jax.lax.scan(micro_step, grads, batch)
+        grads = jax.tree.map(lambda x: x / self.args.opt.grad_accum_steps, grads)
+        outputs = jax.tree.map(lambda x: rearrange(x, "N B ... -> (N B) ..."), outputs)
+        grad_norm = jnp.linalg.norm(jnp.concatenate([g.flatten() for g in jax.tree.leaves(grads)]))
         if train:
             train_state = train_state.apply_gradients(grads=grads)
-        grad_flat = jnp.concatenate([g.flatten() for g in jax.tree.leaves(grads)])
-        grad_norm = jnp.linalg.norm(grad_flat)
-        return train_state, {'loss': metrics['loss_masked'], "grad_norm": grad_norm}
+        return train_state, dict(loss=outputs['loss'].mean(), grad_norm=grad_norm if train else None)
     
+    def do_iter_eval(self, train_state, rng):
+        batch = self.data_gen.generate_batch(rng)
+        batch = jax.tree.map(lambda x: rearrange(x, "(N B) ... -> N B ...", N=self.args.opt.grad_accum_steps), batch)
+        def micro_step(_, batch):
+            loss, outputs = self.loss_fn(train_state.params, batch)
+            return None, outputs
+        _, outputs = jax.lax.scan(micro_step, None, batch)
+        outputs = jax.tree.map(lambda x: rearrange(x, "N B ... -> (N B) ..."), outputs)
+        return train_state, dict(loss=outputs['loss'].mean(), grad_norm=None)
+
     def init(self):
         # self.clip = CLIP()
         rng = jax.random.PRNGKey(self.args.seed)
-        instance = self.generate_instance(rng)
-        print(self.net.tabulate(rng, rng, instance['state']))
-        self.init_params = self.net.init(rng, rng, instance['state'])
+        instance = self.data_gen.generate_instance(rng)
+        print(self.model.tabulate(rng, rng, instance['state']))
+        self.init_params = self.model.init(rng, rng, instance['state'])
         n_params = sum(x.size for x in jax.tree.leaves(self.init_params))
         print(f"Number of parameters: {n_params:,}")
 
+        lr_final = self.args.opt.learning_rate/10 if self.args.opt.decay_lr else self.args.opt.learning_rate
+        lr_schedule = optax.warmup_cosine_decay_schedule(init_value=0., peak_value=self.args.opt.learning_rate,
+                                                         warmup_steps=self.args.n_iters//100,
+                                                         decay_steps=self.args.n_iters-self.args.n_iters//100,
+                                                         end_value=lr_final, exponent=1.)
         tx = optax.chain(optax.clip_by_global_norm(self.args.opt.clip_grad_norm),
-                         optax.adamw(self.args.opt.learning_rate, weight_decay=self.args.opt.weight_decay, eps=1e-8))
-        self.train_state = TrainState.create(apply_fn=self.net.apply, params=self.init_params, tx=tx)
+                         optax.adamw(learning_rate=lr_schedule, weight_decay=self.args.opt.weight_decay,
+                                     b1=self.args.opt.beta1, b2=self.args.opt.beta2, eps=self.args.opt.eps))
+        self.train_state = TrainState.create(apply_fn=self.model.apply, params=self.init_params, tx=tx)
 
         self.final_loss = np.inf
         self.loss_history, self.grad_norm_history = [], []
